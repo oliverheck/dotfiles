c146c60398e77bffa23c20d332826d6a0b932ae0
Oliver Heck <5255945+oliverheck@users.noreply.github.com>
2026-01-19 18:18:58 +0100

    nvim config updates from laptop

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,11 +1,13 @@
 return {
 	"lervag/vimtex",
 	config = function()
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_method = "general"
+		vim.g.vimtex_view_general_viewer = "okular"
+		vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 		vim.g.vimtex_compiler_method = "latexmk"
 		vim.g.vimtex_compiler_latexmk = {
 			options = {
-				"-xelatex",
+				"-lualatex",
 				"-verbose",
 				"-file-line-error",
 				"-synctex=1",
