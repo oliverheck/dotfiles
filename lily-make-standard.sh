411376bd3917cb8cabe97127bb4ec26830154e05
Oliver Heck <5255945+oliverheck@users.noreply.github.com>
2026-02-13 17:10:19 +0100

    Added lily-make-standard.sh

---
+++ lily-make-standard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Verwendung: $0 <Titel>"
+  exit 1
+fi
+
+TITEL="$1"
+QUELLE="/home/oliver/lilypond/templates/template-jazz-standard.ly"
+ZIEL="/home/oliver/lilypond/jazz-standards/${TITEL}.ly"
+
+cp "$QUELLE" "$ZIEL" && echo "Erstellt: $ZIEL"
