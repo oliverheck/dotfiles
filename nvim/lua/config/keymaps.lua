81f040d55ca11b58d71a6ddaaff43db9be50a2ee
Oliver Heck <5255945+oliverheck@users.noreply.github.com>
2026-02-15 07:35:45 +0100

    Umstrukturiert

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,12 @@ vim.g.maplocalleader = ","
 -- Escape alternatives
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+-- Pfeiltasten im Normal-, Insert- und Visual-Mode deaktivieren
+local modes = { "n", "i", "v" }
+for _, mode in ipairs(modes) do
+	vim.keymap.set(mode, "<Up>", "<Nop>", { desc = "Pfeiltaste deaktiviert" })
+	vim.keymap.set(mode, "<Down>", "<Nop>", { desc = "Pfeiltaste deaktiviert" })
+	vim.keymap.set(mode, "<Left>", "<Nop>", { desc = "Pfeiltaste deaktiviert" })
+	vim.keymap.set(mode, "<Right>", "<Nop>", { desc = "Pfeiltaste deaktiviert" })
+end
